660faa8d5b51e90edb6f6bd29d0842dde8bfeeb7
Laurent Mathieu <laurent.mathieu.fr@gmail.com>
2025-11-11 16:09:40 +0300

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,6 +3,6 @@
 sudo yum update -y
 
 # Ensure deployment directory exists and permissions are correct
-mkdir -p /home/ec2-user/flask-app
-chown ec2-user:ec2-user /home/ec2-user/flask-app
-chmod 755 /home/ec2-user/flask-app
+sudo mkdir -p /home/ec2-user/flask-app
+sudo chown ec2-user:ec2-user /home/ec2-user/flask-app
+sudo chmod 755 /home/ec2-user/flask-app
