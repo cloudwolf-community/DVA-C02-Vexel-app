9f75cc15e915f5050226d5cb88dda7e3d6e467b6
Laurent Mathieu <laurent.mathieu.fr@gmail.com>
2025-11-11 19:13:35 +0300

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo unzip -o /home/ec2-user/flask-app/deployment-package.zip -d /home/ec2-user/flask-app/
-sudo chmod +x /home/ec2-user/flask-app/app/app.py
+unzip -o /home/ec2-user/flask-app/deployment-package.zip -d /home/ec2-user/flask-app/
+chmod +x /home/ec2-user/flask-app/app/app.py
 
 # Install  application dependencies
 python3 -m pip install -r /home/ec2-user/flask-app/requirements.txt
