81d6fc84bb18c4e8c67d6c87edcea1844f9de521
Laurent Mathieu <laurent.mathieu.fr@gmail.com>
2025-11-11 17:14:19 +0300

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Copy systemd unit file
-sudo cp scripts/flask-app.service /etc/systemd/system/flask-app.service
+sudo cp /home/ec2-user/flask-app/scripts/flask-app.service /etc/systemd/system/flask-app.service
 sudo chmod 644 /etc/systemd/system/flask-app.service
 
 # Reload systemd to recognize new service
