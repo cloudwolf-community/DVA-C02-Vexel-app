5c98bad8c5d94bfab31fb7f24536bbbf5a4b68b8
Laurent Mathieu <laurent.mathieu.fr@gmail.com>
2025-11-11 16:55:26 +0300

    updated

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-unzip -o /home/ec2-user/flask-app/deployment-package.zip -d /home/ec2-user/flask-app/
+
+# Copy systemd unit file
+sudo cp scripts/flask-app.service /etc/systemd/system/flask-app.service
+sudo chmod 644 /etc/systemd/system/flask-app.service
+
+# Reload systemd to recognize new service
+sudo systemctl daemon-reload
--- scripts/setup_systemd.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Copy systemd unit file
-sudo cp scripts/flask-app.service /etc/systemd/system/flask-app.service
-sudo chmod 644 /etc/systemd/system/flask-app.service
-
-# Reload systemd to recognize new service
-sudo systemctl daemon-reload
