4b671a21a01c16cae2e8b771b2d86a9e3ebb0a92
Laurent Mathieu <laurent.mathieu.fr@gmail.com>
2025-11-12 12:01:05 +0300

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,4 +3,4 @@
 sudo chmod +x /home/ec2-user/flask-app/app/app.py
 
 # Install  application dependencies
-python3 -m pip install -r /home/ec2-user/flask-app/requirements.txt
+sudo python3 -m pip install -r /home/ec2-user/flask-app/requirements.txt
